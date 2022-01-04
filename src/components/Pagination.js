cca121201894df5a0ab633405c140e1cce304261
bintangpananjung <68179028+bintangpananjung@users.noreply.github.com>
2022-01-04 15:23:27 +0700

    Update Pagination.js

--- src/components/Pagination.js
+++ src/components/Pagination.js
@@ -4,21 +4,38 @@ import { Link } from "react-router-dom";
 const Pagination = props => {
   const pager = (maxPage, page) => {
     var item = [];
-    const pagecount = maxPage > 5 ? 5 : maxPage;
-    for (let i = page - 1; i < pagecount; i++) {
+    var pagecount;
+    var init;
+    if (maxPage >= 5) {
+      if (maxPage - page >= 4) {
+        pagecount = page + 4;
+        init = page - 1;
+      } else {
+        if (page > maxPage - 5) {
+          pagecount = maxPage;
+          init = maxPage - 5;
+        }
+      }
+    } else {
+      pagecount = maxPage;
+      init = 0;
+    }
+
+    for (let i = init; i < pagecount; i++) {
       item.push(
         <li className="page-item" key={i}>
           <Link
             className={
               (page == i + 1 ? "active" : "") + " page-link pagination-item"
             }
-            to={`page=${i + 1}`}
+            to={`?page=${i + 1}`}
           >
             {i + 1}
           </Link>
         </li>
       );
     }
+
     return item;
   };
   return (
